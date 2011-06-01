b78abc982b0be925408acd47ce2c5cf04694b38a
Myron Turner <turnermm02@shaw.ca>
2011-05-31 22:57:39 -0500

    Added to syntax.php the ability to include other macros inside a primary macro.

--- syntax.php
+++ syntax.php
@@ -20,7 +20,7 @@ define('MACROS_FILE', REPLACE_DIR . 'macros.ser');
  * need to inherit from this class
  */
 class syntax_plugin_textinsert extends DokuWiki_Syntax_Plugin {
-
+   var $macros;
     /**
      * return some info
      */
@@ -67,12 +67,13 @@ class syntax_plugin_textinsert extends DokuWiki_Syntax_Plugin {
         $match = trim($match);   
         if(strpos($match, 'HTML')) $html=true;
         file_put_contents(DOKU_INC ."textinsert.txt", $match);
-        $macros = $this->get_macros();
-        if(!array_key_exists($match, $macros)) {
+        $this->macros = $this->get_macros();
+        if(!array_key_exists($match, $this->macros)) {
            msg("$match macro was not found in the macros database", -1);  
            $match = "";              
         }
-        else $match =$macros[$match];
+        else $match =$this->macros[$match];
+        $match = $this->get_inserts($match); 
         if($html) {
           $match =  str_replace('&lt;','<',$match);
           $match =  str_replace('&gt;','>',$match);
@@ -99,6 +100,20 @@ class syntax_plugin_textinsert extends DokuWiki_Syntax_Plugin {
        return array();
     }
 
+   function get_inserts($match) {
+      $inserts = array();
+
+      if(preg_match_all('/#@(.*?)@#/',$match,$inserts)) {        
+          $keys = $inserts[1]; 
+          $pats = $inserts[0];           
+          for($i=0; $i<count($keys); $i++) {
+            $insert = $this->macros[$keys[$i]];
+            $match = str_replace($pats[$i],$insert,$match);
+          }
+      }
+
+      return  $match;
+   }
    
 }
 

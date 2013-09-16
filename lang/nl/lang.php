d157a75c12e8dd526a78558dc055a871dfda1beb
Myron Turner <turnermm02@shaw.ca>
2013-09-16 14:12:51 -0500

    revised nl/lang.php

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -28,23 +28,3 @@ $lang['hide_macros'] = 'Macro Lijst Verbergen';
 $lang['hide_info'] = 'Info Sluiten';
 $lang['show_info'] = 'Info Tonen';
 
-$lang['msg'] = 'Met dit luik kan je macro\'s en hun vervangingsteksten onderhouden: aanmaken, schrappen en  
-bewerken nadat ze opgeslagen zijn. Een macro naam kan letters, cijfers, onderlijningsstreepjes, koppeltekens 
-en punten bevatten. Voorbeeld:&nbsp;&nbsp;<code>Macro_one.txt</code>
-De vervangtekst kan in HTML zijn en gelijk welke lengte.
-<p>
-Zes macro\'s tegelijk kan je toevoegen. De zesde is de tekstzone waar je uitgebreide teksten kan invoeren.
-Geef een macro naam in de <b>Macro</b> kolom en de tekst die hier aan gekoppeld is in de
-<b>Substitutie</b> kolom. 
-</p>
-<p>
-Schrappen kan je onbeperkt uitvoeren. Vink het vakje of de vakjes aan naast de macro\'s die geschrapt moeten
-worden. Klik daarna op de Schrappen knop onderaan het scherm. Bewerken doe je via het bewerkingsscherm waar
-je alle macro\'s ziet staan. Je bewerkt teksten van zoveel macro\'s als je maar wil. 
-</p>
-<p>
-De macro lijst wordt niet hernieuwd zolang de wijzigingen, toevoegingen, schrappingen niet doorgevoerd zijn
-door te klikken op de voorziene knop onderaan het scherm.
-</p>
-';
-

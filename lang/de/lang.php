d921356377fa643df398f9eecd3405faf0b2be7b
Thor Weinreich <thorweinreich@nefkom.net>
2014-03-18 00:15:05 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
+ */
+$lang['btn_add']               = 'Hinzufügen';
+$lang['btn_del']               = 'Löschen';
+$lang['btn_edit']              = 'Änderungen speichern';
+$lang['label_list']            = 'Makro-Liste';
+$lang['label_add']             = 'Makros hinzufügen';
+$lang['label_del']             = 'Makros löschen';
+$lang['label_edit']            = 'Makros bearbeiten';
+$lang['col_subst']             = 'Makros ersetzen';
+$lang['add_macros']            = 'Makros hinzufügen';
+$lang['delete_macros']         = 'Makros löschen';
+$lang['edit_macros']           = 'Makros bearbeiten';
+$lang['view_macros']           = 'Makro-Liste anzeigen';
+$lang['hide_macros']           = 'Makro-Liste verbergen';
+$lang['hide_info']             = 'Info schließen';
+$lang['show_info']             = 'Info anzeigen';

0d186a1abab9522b0d9e95bd82bb5e2358bf8652
Ner0lph <forums@ner0lph.tk>
2015-03-24 02:35:06 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ner0lph <forums@ner0lph.tk>
+ */
+$lang['menu']                  = 'TextInsert Macro Replacement Plugin';
+$lang['btn_add']               = 'Ajouter';
+$lang['btn_del']               = 'Supprimer';
+$lang['btn_edit']              = 'Envoyer les modifications';
+$lang['label_list']            = 'Liste des macros';
+$lang['label_add']             = 'Ajouter des macros';
+$lang['label_del']             = 'Supprimer des macros';
+$lang['label_edit']            = 'Modifier des macros';
+$lang['col_subst']             = 'Substitution de macro';
+$lang['add_macros']            = 'Ajouter des macros';
+$lang['delete_macros']         = 'Supprimer des macros';
+$lang['edit_macros']           = 'Modifier des macros';
+$lang['view_macros']           = 'Afficher la liste des macros';
+$lang['hide_macros']           = 'Cacher la liste des macros';
+$lang['hide_info']             = 'Fermer l\'info';
+$lang['show_info']             = 'Montrer l\'info';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ner0lph <forums@ner0lph.tk>
+ */
+$lang['stdreplace']            = 'Accepter des remplacements standards de macros en tant que modèles de catégorie';

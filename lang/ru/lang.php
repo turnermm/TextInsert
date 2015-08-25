e41e79a6fe16a215f9f9756c9f787ecfaba0799a
RainbowSpike <1@2.ru>
2015-08-25 08:45:06 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author RainbowSpike <1@2.ru>
+ */
+$lang['btn_add']               = 'Добавить';
+$lang['btn_del']               = 'Удалить';
+$lang['btn_edit']              = 'Подтвердить правки';
+$lang['label_list']            = 'Список макросов';
+$lang['label_add']             = 'Добавить макрос';
+$lang['label_del']             = 'Удалить макрос';
+$lang['label_edit']            = 'Изменить макрос';
+$lang['add_macros']            = 'Добавить макрос';
+$lang['delete_macros']         = 'Удалить макрос';
+$lang['edit_macros']           = 'Изменить макрос';
+$lang['view_macros']           = 'Показать макросы';
+$lang['hide_macros']           = 'Скрыть список';
+$lang['hide_info']             = 'Скрыть справку';
+$lang['show_info']             = 'Показать справку';
---
+++ lang/ru/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author RainbowSpike <1@2.ru>
+ */
+$lang['stdreplace']            = 'Допускать переменные из папочных шаблонов _template.txt';

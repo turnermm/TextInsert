cfc4c867b81ec8d2084e3542635c3c0d48d9df1d
Aleksandr Selivanov <alexgearbox@yandex.ru>
2016-02-27 08:30:30 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -4,5 +4,6 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author RainbowSpike <1@2.ru>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-$lang['stdreplace']            = 'Допускать переменные из папочных шаблонов _template.txt';
+$lang['stdreplace']            = 'Допускать стандартные переменные, используемые для шаблонов пространств имён';

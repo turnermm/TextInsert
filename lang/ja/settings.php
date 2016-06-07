b5d565631fc15eca21b057303cdd98fc4c7fcf6b
DokuWiki Translation Tool <admin@dokuwiki.org>
2016-06-07 03:26:48 +0200

    translation update (#12)

---
+++ lang/ja/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['stdreplace']            = '名前空間テンプレートで使用されている標準マクロ置換を許容する';

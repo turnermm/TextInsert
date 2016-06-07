669eea98e8cab502231b94319d5519b9be5b163c
Hideaki SAWADA <chuno@live.jp>
2016-06-07 03:15:31 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = 'TextInsert マクロ置換プラグイン';
+$lang['btn_add']               = '追加';
+$lang['btn_del']               = '削除';
+$lang['btn_edit']              = '編集の実行';
+$lang['label_list']            = 'マクロ一覧';
+$lang['label_add']             = 'マクロ追加';
+$lang['label_del']             = 'マクロ削除';
+$lang['label_edit']            = 'マクロ編集';
+$lang['col_subst']             = 'マクロ 置換';
+$lang['add_macros']            = 'マクロ追加';
+$lang['delete_macros']         = 'マクロ削除';
+$lang['edit_macros']           = 'マクロ編集';
+$lang['view_macros']           = 'マクロ一覧を表示';
+$lang['hide_macros']           = 'マクロ一覧を非表示';
+$lang['hide_info']             = '解説を非表示';
+$lang['show_info']             = '解説を表示';

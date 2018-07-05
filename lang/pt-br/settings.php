d0c41ce155b373b93652986fbf019bf9abc0b2d4
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 19:10:38 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['stdreplace']            = 'Aceite substituições de macro padrão como usadas em modelos de namespace';
+$lang['farm']                  = 'Se isto é uma fazenda e você e gostaria que cada animal tivesse seu próprio banco de dados de macros, selecione essa opção; caso contrário, todos os animais compartilharão o mesmo banco de dados (ou seja, do fazendeiro).';

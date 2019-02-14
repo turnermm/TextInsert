b78746fb37e574d9ea94a90743738202adcf4c30
RainSlide <RainSlide@outlook.com>
2019-02-14 18:05:27 +0100

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author RainSlide <RainSlide@outlook.com>
+ */
+$lang['menu']                  = 'TextInsert 宏插件';
+$lang['btn_add']               = '添加';
+$lang['btn_del']               = '删除';
+$lang['btn_edit']              = '提交编辑';
+$lang['label_list']            = '宏列表';
+$lang['label_add']             = '添加宏';
+$lang['label_del']             = '删除宏';
+$lang['label_edit']            = '编辑宏';
+$lang['col_subst']             = '替换文本';
+$lang['add_macros']            = '添加宏';
+$lang['delete_macros']         = '删除宏';
+$lang['edit_macros']           = '编辑宏';
+$lang['view_macros']           = '显示宏列表';
+$lang['hide_macros']           = '隐藏宏列表';
+$lang['hide_info']             = '关闭信息';
+$lang['show_info']             = '显示信息';

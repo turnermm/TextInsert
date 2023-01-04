089f720af3dc45e94d0d6fc5b70d1a317692b550
Hella <hella.breitkopf@gmail.com>
2023-01-04 12:50:06 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author Thor Weinreich <thorweinreich@nefkom.net>
  */
+$lang['menu']                  = 'TextInsert Plugin für Text-Makros';
 $lang['btn_add']               = 'Hinzufügen';
 $lang['btn_del']               = 'Löschen';
 $lang['btn_edit']              = 'Änderungen speichern';
@@ -20,3 +22,4 @@ $lang['view_macros']           = 'Makro-Liste anzeigen';
 $lang['hide_macros']           = 'Makro-Liste verbergen';
 $lang['hide_info']             = 'Info schließen';
 $lang['show_info']             = 'Info anzeigen';
+$lang['not_found']             = 'Makro wurde nicht in der Makro-Datenbank gefunden';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,7 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author Thor Weinreich <thorweinreich@nefkom.net>
  */
 $lang['stdreplace']            = 'Standard Makro-Ersetzungen annehmen, wie sie in Namensraum-Templates benutzt werden';
+$lang['farm']                  = 'Wenn dies eine Wiki-Farm ist und jedes Wiki seine eigene Makro-Datenbank haben soll, diese Option wählen. Sonst verwenden alle Wikis die Datenbank des Wiki-Farmers';
